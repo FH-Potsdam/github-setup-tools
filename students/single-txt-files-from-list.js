96bf47f3d922033314980503044ad6b430bb30b0
fabiantheblind <icke@fabianmoronzirfas.me>
2014-04-11 11:18:57 +0200

    modified:   package.json modified:   single-txt-files-from-list.js new file:   students-list_example.txt

--- students/single-txt-files-from-list.js
+++ students/single-txt-files-from-list.js
@@ -1,21 +1,35 @@
-var fs = require('fs');
-
+var fs = require('fs'); // need this for writing files
+var mkdirp = require('mkdirp');
+// whitespace trimmer found here
+// stackoverflow: https://tinyurl.com/o8cll7v
 var trimmer = function (str) {
   return str.replace(/^\s\s*/, '')
     .replace(/\s\s*$/, '');
 };
+
+// see the example list file for reference
 var list = fs.readFileSync("_private/students-list.txt", 'utf8')
   .split('\n');
 
+mkdirp('_private/one-per-file', function (err) {
+  if (err) console.error(err);
+  else console.log('pow!');
+});
+
 // console.log(list);
+//
+// start the loop at 1 the 0 is the header
+for (var i = 1; i < list.length; i++) {
+  var name = list[i].split(',')[0]; // first entry is the real name
 
-for (var i = 0; i < list.length; i++) {
-  var name = list[i].split(',')[0];
-
+  // make name lowercase, then trimm whitespaces
+  // and then replace whitespaces with undersocres
   var fn = trimmer(name.toLowerCase())
     .replace(/\s+/g, '_');
 
-  fs.writeFileSync("_private/one-per-file/" + fn + ".txt", list[i], "UTF-8", {
+
+  // write file with the header and the line
+  fs.writeFileSync("_private/one-per-file/" + fn + ".txt", list[0] + "\n" + list[i], "UTF-8", {
     'flags': 'w+'
   });
 }
